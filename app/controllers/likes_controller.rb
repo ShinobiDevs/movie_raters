dd35d53bc1a779def020c70f59e401f9d2115724
Elad Meidar <elad@eizesus.com>
2013-07-24 18:52:03 +0300

    [TESTS] Green

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,36 +4,42 @@ class LikesController < ApplicationController
   # GET /users/1/likes.json
   # GET /users/1/likes.xml
   def index
-    @user = User.find(params[:user_id])
+    @user = User.where(id: params[:user_id]).first
     respond_to do |wants|
       if @user.present?
         response_hash = {likes: @user.liked_movies_ids }
         wants.xml { render xml: response_hash }
         wants.json { render json: response_hash }
       else
-        wants.xml { render nothing: true, status: :not_found}
+        wants.xml { render nothing: true, status: :not_found }
         wants.json { render nothing: true, status: :not_found }
       end
     end
   end
 
-  def create 
-    @user.like!(@movie)
-    render nothing: true, status: :created
+  def create
+    if @user.like!(@movie)
+      render nothing: true, status: :created
+    else
+      render nothing: true, status: :not_modified
+    end
   end
 
   def destroy
-    @user.unlike!(@movie)
-    render nothing: true, status: :ok
+    if @user.unlike!(@movie)
+      render nothing: true, status: :ok
+    else
+      render nothing: true, status: :not_modified
+    end
   end
 
   protected
 
   def find_user_and_movie
-    @user = User.find(params[:user_id])
-    @movie = Movie.find(params[:movie_id])
+    @user = User.where(id: params[:user_id]).first
+    @movie = Movie.where(id: params[:id]).first if params.has_key?(:id)
 
-    if @user.blank? || @movie.blank?
+    if @user.blank? || (@movie.blank? && params.has_key?(:id))
       render nothing: true, status: :not_found
       return
     end
--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -13,7 +13,58 @@ describe LikesController do
       @user.like!(@movie)
       get :index, {user_id: @user.id, format: :json}
       response.code.should eq("200")
+      JSON.parse(response.body)["likes"].should eq([@movie.id.to_s])
+    end
+
+    it "should respond with 404 for a non existant user" do
+      get :index, {user_id: 12312332, format: :json}
+      response.code.should eq("404")
     end
   end
 
+  describe "#create" do
+    it "should respond with '201 Created' for a valid user and movie like" do
+      post :create, {user_id: @user.id, id: @movie.id, format: :json}
+      response.code.should eq("201")
+    end
+
+    it "should respond with a 404 for a non existant user" do
+      post :create, {user_id: 12312332, id: @movie.id, format: :json}
+      response.code.should eq("404")
+    end
+
+    it "should respond with a 404 for a non existant movie" do
+      post :create, {user_id: @user.id, id: 1231232, format: :json}
+      response.code.should eq("404")
+    end
+
+    it "should respond with a '304 Not modified' for an already liked movie" do
+      @user.like!(@movie)
+      post :create, {user_id: @user.id, id: @movie.id, format: :json}
+      response.code.should eq("304")
+    end
+  end
+
+  describe "#destroy" do
+    it "should respond with '200 Created' for a valid user and movie unlike" do
+      @user.like!(@movie)
+      delete :destroy, {user_id: @user.id, id: @movie.id, format: :json}
+      response.code.should eq("200")
+    end
+
+    it "should respond with a 404 for a non existant user" do
+      delete :destroy, {user_id: 12312332, id: @movie.id, format: :json}
+      response.code.should eq("404")
+    end
+
+    it "should respond with a 404 for a non existant movie" do
+      delete :destroy, {user_id: @user.id, id: 1231232, format: :json}
+      response.code.should eq("404")
+    end
+
+    it "should respond with a '304 Not modified' for an already unliked movie" do
+      delete :destroy, {user_id: @user.id, id: @movie.id, format: :json}
+      response.code.should eq("304")
+    end
+  end
 end
